af96683930797516ff275de50792912d2c4c5ed3
DipakShrestha-ADS <dipak.shrestha@eemc.edu.np>
2026-06-04 12:55:24 +0545

    feat: add validation functions for student fields and enhance error handling in student operations

--- src/handlers/handler.js
+++ src/handlers/handler.js
@@ -1,68 +1,128 @@
 import prisma from "../db/prisma.js"
+import { ValidateAllFieldTypes, ValidateEmptyField } from "../validators/field_validator.js"
 let FindAllStudents = async (req, res) => {
-    let allStudents = await prisma.students.findMany()
-    res.json({
-        message: "all students found",
-        data: allStudents
-    })
+    try {
+        let allStudents = await prisma.students.findMany()
+        res.json({
+            message: "all students found",
+            data: allStudents
+        })
+    } catch (e) {
+        res.status(500).json({
+            error: "Something went wrong",
+            stack: e?.message
+        })
+    }
 }
 let FindStudentById = async (req, res) => {
-    let matchStudent = await prisma.students.findUnique({
-        where: {
-            id: Number(req.params.id),
+    try {
+        let id = req.params.id
+        // empty validation with bad request status
+        if (id === ""){
+            res.status(400).json({
+                error: "id cannot be empty",
+            })
+            return
         }
-    })
-    res.status(200).json({
-        message: "student found",
-        data: matchStudent
-    })
+        // check if id is number or not and must return status related to it
+        if (isNaN(id)){
+            res.status(400).json({
+                error: "id must be a number",
+            })
+            return
+        }
+        let matchStudent = await prisma.students.findUnique({
+            where: {
+                id: Number(req.params.id),
+            }
+        })
+        res.status(200).json({
+            message: "student found",
+            data: matchStudent
+        })
+    } catch (e) {
+        res.status(500).json({
+            error: "Something went wrong",
+            stack: e?.message
+        })
+    }
 }
 let CreateStudent = async (req, res) => {
-    let data = req.body
-    let createdStudent = await prisma.students.create({
-        data: data
-    })
-    res.status(201).json({
-        message: "student created successfully",
-        data: createdStudent
-    })
+    try {
+        let data = req.body
+        let {email, name, roll_no} = data
+        let validateMsg = ValidateAllFieldTypes("email", email)
+        if(validateMsg != null){
+            res.status(400).json({
+                error: validateMsg
+            })
+            return
+        }
+        let validateMsg = ValidateAllFieldTypes("name", name)
+        if(validateMsg != null){
+            res.status(400).json({
+                error: validateMsg
+            })
+            return
+        }
+        let createdStudent = await prisma.students.create({
+            data: data
+        })
+        res.status(201).json({
+            message: "student created successfully",
+            data: createdStudent
+        })
+    } catch (e) {
+        res.status(500).json({
+            error: "Something went wrong",
+            stack: e?.message
+        })
+    }
 }
 let UpdateStudent = async (req, res) => {
-    let id = req.params.id
-    let { name, email, roll_no } = req.body
-    let updatedStudent = await prisma.students.update(
-        {
-            where: {
-                id: Number(id),
-            },
-            data: {
-                name,
-                email,
-                rollNo: roll_no,
+    try {
+        let id = req.params.id
+        let { name, email, roll_no } = req.body
+        let updatedStudent = await prisma.students.update(
+            {
+                where: {
+                    id: Number(id),
+                },
+                data: {
+                    name,
+                    email,
+                    rollNo: roll_no,
+                }
             }
-        }
-    )
-    res.status(200).json({
-        message: "udpated",
-        data: UpdateStudent
-    })
+        )
+        res.status(200).json({
+            message: "udpated",
+            data: UpdateStudent
+        })
+    } catch (e) {
+        res.status(500).json({
+            error: "cannot update student",
+            stack: e?.message
+        })
+    }
 }
+github.com / DipakShrestha - ADS / student_record.git
 let DeleteStudent = async (req, res) => {
     try {
         let id = req.params.id
         let deletedStudent = await prisma.students.delete({
             where: {
-                id: id
+                id: Number(id)
             }
         })
         res.status(200).json({
             message: `Student with id ${id} deleted successfully.`,
             data: deletedStudent
         })
-    }catch(e){
+    } catch (e) {
         res.status(500).json({
             error: "Something went wrong",
-            stack: e
+            stack: e?.message
         })
     }
 }
