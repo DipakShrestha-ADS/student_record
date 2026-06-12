28a17b54a2e75e5804383335ab78329e11b4a3da
DipakShrestha-ADS <dipak.shrestha@eemc.edu.np>
2026-06-12 12:56:49 +0545

    teachers api refractor

--- src/handlers/teacher.controller.js
+++ src/handlers/teacher.controller.js
@@ -2,12 +2,64 @@ import prisma from "../db/prisma.js";
 
 const getAllTeachers = async (req, res) => {
   const teachers = await prisma.teacher.findMany({
-    include: { department: true, courses: true },
+    include: { department: {
+      select:{
+        id: true,
+        name: true,
+      }
+    }, courses: true },
   });
   res
     .status(200)
     .json({ message: "All teachers fetched successfully", data: teachers });
 };
+// using prisma relation select
+export const getAllTeachersWithSelect = async (req, res) => {
+  const teachers = await prisma.teacher.findMany({
+    select:{
+      name: true,
+      id: true,
+      createdAt: true,
+      department: {
+        select: {
+          // name: true,
+          id: true
+        }
+      },
+      courses: true,
+    }
+  });
+  res
+    .status(200)
+    .json({ message: "All teachers fetched successfully", data: teachers });
+};
+
+// sorting wiht orderBy
+let sortTeachers = async(req,res)=>{
+  let teachers = await prisma.teacher.findMany({
+    orderBy:{
+      name: "desc"
+    }
+  })
+  res.status(200).json({
+    message: "sorted teachers",
+    data: teachers
+  })
+}
+// filtering:
+let filterTeachers = async(req,res)=>{
+  let teachers = await prisma.teacher.findMany({
+    where: {
+      name:{
+        gte: "a"
+      }
+    }
+  })
+  res.status(200).json({
+    message: "sorted teachers",
+    data: teachers
+  })
+}
 
 const findTeacherById = async (req, res) => {
   const { id } = req.params;
@@ -31,6 +83,25 @@ const createTeacher = async (req, res) => {
       email,
       department: {
         connect: { id: Number(departmentId) },
+      },
+    },
+  });
+  res.status(201).json({
+    message: "Teacher created successfully",
+    data: newTeacher,
+  });
+};
+// using create prisma relation
+export const createTeacherWithDepartment = async (req, res) => {
+  const { name, email, departmentName } = req.body;
+  const newTeacher = await prisma.teacher.create({
+    data: {
+      name,
+      email,
+      department: {
+        create: {
+          name: departmentName
+        }
       },
     },
   });
--- src/routes/teacher.routes.js
+++ src/routes/teacher.routes.js
@@ -1,17 +1,22 @@
+import { getAllStudentsWithSelect } from "../handlers/student.controller.js";
 import {
   createTeacher,
   findTeacherById,
   getAllTeachers,
   updateTeacher,
   deleteTeacher,
+  createTeacherWithDepartment,
+  getAllTeachersWithSelect,
 } from "../handlers/teacher.controller.js";
 import { Router } from "express";
 
 let router = Router();
 
 router.get("/", getAllTeachers);
-router.get("/:id", findTeacherById);
+router.get("/single/:id", findTeacherById);
+router.get("/select", getAllTeachersWithSelect)
 router.post("/", createTeacher);
+router.post("/with-dpart", createTeacherWithDepartment);
 router.put("/:id", updateTeacher);
 router.delete("/:id", deleteTeacher);
 
